33b06f9ede0ff25b3d39b22bb8d9ee094a415396
Sumir Vats <sumirvats@gmail.com>
2023-07-19 20:12:48 +0530

    Login Page and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'ch6.dart';
+import 'pages/login_page.dart';
 // import 'practice2.dart';
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       // home: const Practice2(),
-      home: const Ch6(),
+      home: const LoginPage(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
